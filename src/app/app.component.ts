6968bd639034f3f0ea1b57cf0d5fa01c17d3621c
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-05-17 15:41:16 +0300

    add angular base project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'RSS/Atom reader for Master of Code';
+}
