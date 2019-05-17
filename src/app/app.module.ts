780fe685ebc8fc13f672326f67d6af88aaf70de0
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-05-17 17:04:24 +0300

    Add ng-zorro-antd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChannelsComponent } from './channels/channels.component';
+import { ItemsComponent } from './items/items.component';
+import { SummaryComponent } from './summary/summary.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChannelsComponent,
+    ItemsComponent,
+    SummaryComponent,
+    StatisticsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
