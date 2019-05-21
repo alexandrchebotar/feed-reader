01e838b978e6412aa0db4097f9d18514da90630f
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-05-21 07:06:03 +0300

    Add HTML sanitizing

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Item } from '../data.service';
 
-@Component({
+@Component ({
   selector: 'app-description',
   templateUrl: './description.component.html',
   styleUrls: ['./description.component.css']
 })
 export class DescriptionComponent {
-  @Input() item: Item;
+  @Input() item: Item;  
 }
