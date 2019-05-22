a32fa7f577abf44017e0d56381ac181c65898e77
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-05-23 01:48:33 +0300

    Fix chart colors bub

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -14,6 +14,8 @@ export class ChartComponent implements OnInit {
   options = {
     legend: {position: 'left'}, 
   };
+  pieChartColors: string[];
+  pieChartDataset: any[];
   constructor(private _statistics: StatisticsService) {};
 
   ngOnInit() {
@@ -21,6 +23,12 @@ export class ChartComponent implements OnInit {
       this.letters = res.sort((a, b) => a.letter > b.letter ? 1 : -1);
       this.pieChartLabels = this.letters.map(letterStats => letterStats.letter);
       this.pieChartData = this.letters.map(letterStats => letterStats.entries);
+      this.pieChartColors = this.letters.map(() => this.randomColor())
+      this.pieChartDataset = [{data: this.pieChartData, backgroundColor: this.pieChartColors}]
     });
+  }
+  randomColor() {
+    const randomFrom255 = () => Math.trunc(256*Math.random());
+    return `rgba(${randomFrom255()}, ${randomFrom255()}, ${randomFrom255()}, 0.5)`;
   }
 }
